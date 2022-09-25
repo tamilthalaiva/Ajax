4c6fca6981405d09a16fa8fe1c03a96f20d5ff79
ridonebabat <73724343+ridonebabat@users.noreply.github.com>
2022-09-25 17:02:18 +0100

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -398,8 +398,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/+WcQKaiX2wyFmNzk0'),
-            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/+WcQKaiX2wyFmNzk0')
+            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/+knbq3NvmqR5hNzg8'),
+            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/+knbq3NvmqR5hNzg8')
             ],[      
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('CONTACT ME', url='https://t.me/ridonebabat'),
